0df27ecc8b4c7828fea861e54c5fa89426e3cbb5
Oleksand Sonich <tbunitrade@gmail.com>
2018-12-02 17:58:43 +0200

    MVP ready to play

--- vanilla-script.js
+++ vanilla-script.js
@@ -1,284 +1,294 @@
-window.onload = function() {
+window.onload = function () {
 
-    var firstPlayer, secondPlayer, trigger,  startGame, // start game
-    startContainer, // game container DELETE?
-    checkNames, setname,
-    playButton, videoContent, rolik, video, gameIndex, j, nuchya;
+    let trigger, // start game
+        playButton, videoContent, rolik, video, gameIndex, resetHandlers;
 
-    checkNames = false;
-
-
-    var playerS = new Object();   
-    playerS = {
-        playerOne : {
-            name : 'name1',
-            score : 0
+    let playerS = {
+        playerOne: {
+            name: 'name1',
+            score: 0
         },
-        playerTwo : {
-            name : 'name2',
-            score : 0
+        playerTwo: {
+            name: 'name2',
+            score: 0
         }
     };
-   
-    
-    startGame = document.getElementById('playButton');
-    startContainer = document.getElementById('startGameContainer');
-    
-    document.addEventListener('click', function setTrigger() {   
-       trigger = document.querySelector('input[name="option"]:checked').value;               
-     
 
-        if ( trigger == 0) {
-            //change second label
-            document.getElementById('secondPlayerLabel').innerHTML = 'X';
-        } else {
-            document.getElementById('secondPlayerLabel').innerHTML = '0';
+
+    const startGame = document.getElementById('playButton');
+    const startContainer = document.getElementById('startGameContainer');
+
+    function initGame(event) {
+        const firstPlayer = document.getElementById('firstPlayer').value;
+        const secondPlayer = document.getElementById('secondPlayer').value;
+
+        if (checkNames(firstPlayer, secondPlayer)) {
+            playVideo();
+            setPlayerNames(firstPlayer, secondPlayer);
+            createCells();
+            resetHandlers = bindGameControlsEvents();
         }
-    });
-    
-    //trigger = 
-    
-    // first screen
-    startGame.onclick = function(event) {
-        firstPlayer = document.getElementById('firstPlayer').value;
-        secondPlayer = document.getElementById('secondPlayer').value;     
-
-
-        
-        function checkNames() {
-            console.log('101');
-            if ( (!firstPlayer.length > 0)   ) {
-                alert('Введите именя Игрока № 1 ');             
-            } 
-            
-            else if (!secondPlayer.length > 0){
-                alert('Введите именя Игрока № 2 ');
-            }            
-            else {
-                checkNames = true;           
-
-                if ( trigger == 'x') {
-                    document.getElementById("playerOneName").innerHTML = firstPlayer;
-                    document.getElementById("playerTwoName").innerHTML = secondPlayer;
-                }
-                else {
-                    document.getElementById("playerOneName").innerHTML = secondPlayer;
-                    document.getElementById("playerTwoName").innerHTML = firstPlayer;
-                }                
-            }            
-        }        
-        checkNames();
-
-        if ( checkNames == true)  {       
-
-            videoContent = document.getElementById('contentElement');
-            playButton = document.getElementById('playButton');    
-            playButton.addEventListener('click', onPlayButtonClick);
-                
-            function onPlayButtonClick() {
-                videoContent.play();
-                  
-            }
-
-            startContainer.style.display = "none";
-            onPlayButtonClick();
-            video.style.left="0";
-            setInterval(() => {
-                backStyleStart();
-            }, 11200);         
-            
-            document.addEventListener('keypress', function(event){            
-                var playerOneKeys   =   ['1','2','3','4','5','6','7','8','9'];
-                var playerTwoKeys   =   ['q','w','e','r','t','y','u','i','o'];
-        
-                if ( playerOneKeys.includes(event.key) ) {
-                    document.getElementById(`cell${event.key}`).innerHTML = '+';
-                }
-        
-                if ( playerTwoKeys.includes(event.key) ) {
-                    document.getElementById(`cell${playerTwoKeys.indexOf(event.key)+1}`).innerHTML = '-';
-                }                        
-            });            
-        }      
     }
 
+    function initEntryScreen() {
+        document.addEventListener('click', function setTrigger() {
+            trigger = document.querySelector('input[name="option"]:checked').value;
+
+            if (trigger == 0) {
+                //change second label
+                document.getElementById('secondPlayerLabel').innerHTML = 'X';
+            } else {
+                document.getElementById('secondPlayerLabel').innerHTML = '0';
+            }
+        });
+
+        startGame.onclick = initGame;
+    }
+
+    function checkNames(firstPlayer, secondPlayer) {
+        if ((!firstPlayer.length > 0)) {
+            alert('Введите именя Игрока № 1 ');
+            return false;
+        } else if (!secondPlayer.length > 0) {
+            alert('Введите именя Игрока № 2 ');
+            return false;
+        }
+
+        return true;
+    }
+
+    function setPlayerNames(firstPlayer, secondPlayer) {
+        if (trigger === 'x') {
+            document.getElementById("playerOneName").innerHTML = firstPlayer;
+            document.getElementById("playerTwoName").innerHTML = secondPlayer;
+        }
+        else {
+            document.getElementById("playerOneName").innerHTML = secondPlayer;
+            document.getElementById("playerTwoName").innerHTML = firstPlayer;
+        }
+    }
+
+    function createCells() {
+        for (var cell = 1; cell < 10; cell++) {
+            document.getElementById('game').innerHTML += `<div id="cell${cell}" class="cell" tabindex="1" ></div>`
+        }
+    }
+
+    function isWinner(playerSymbol) {
+        const cells = document.getElementsByClassName('cell');
+
+        return (cells[0].innerHTML === playerSymbol && cells[1].innerHTML === playerSymbol && cells[2].innerHTML === playerSymbol)
+            ||
+            (cells[3].innerHTML === playerSymbol && cells[4].innerHTML === playerSymbol && cells[5].innerHTML === playerSymbol)
+            ||
+            (cells[6].innerHTML === playerSymbol && cells[7].innerHTML === playerSymbol && cells[8].innerHTML === playerSymbol)
+            ||
+            (cells[0].innerHTML === playerSymbol && cells[3].innerHTML === playerSymbol && cells[6].innerHTML === playerSymbol)
+            ||
+            (cells[1].innerHTML === playerSymbol && cells[4].innerHTML === playerSymbol && cells[7].innerHTML === playerSymbol)
+            ||
+            (cells[2].innerHTML === playerSymbol && cells[5].innerHTML === playerSymbol && cells[8].innerHTML === playerSymbol)
+            ||
+            (cells[0].innerHTML === playerSymbol && cells[4].innerHTML === playerSymbol && cells[8].innerHTML === playerSymbol)
+            ||
+            (cells[2].innerHTML === playerSymbol && cells[4].innerHTML === playerSymbol && cells[6].innerHTML === playerSymbol)
+    }
+
+    function calc() {
+        // line horizint x
+        if (isWinner('x')) {
+            const scoreSpan = document.querySelector("#playerOneScore span");
+            const score = scoreSpan.innerHTML;
+            scoreSpan.innerHTML = parseInt(score) + 1;
+
+            function func() {
+                alert('Krestiki Победители');
+            }
+
+            setTimeout(func, 500);
+
+            function myFunction1() {
+                let element = document.getElementById("myDIV1");
+                element.classList.add("mystyle");
+            }
+            myFunction1();
+        }
+
+        if (isWinner('0')) {
+            const scoreSpan = document.querySelector("#playerTwoScore span");
+            const score = scoreSpan.innerHTML;
+            scoreSpan.innerHTML = parseInt(score) + 1;
+
+            function func() {
+                alert('Noliki Победители');
+            }
+
+            setTimeout(func, 500);
+
+            function myFunction2() {
+                let element = document.getElementById("myDIV2");
+                element.classList.add("mystyle");
+            }
+            myFunction2();
+        }
+    }
+
+    function isPlayerXturn(turn) {
+        return turn % 2 !== 0;
+    }
+
+    function renderPlayerTurn(selectedCell, turn) {
+        if (isPlayerXturn(turn)) {
+            selectedCell.innerHTML = 'x';
+            selectedCell.style.backgroundColor = '#0099ff';
+        } else {
+            selectedCell.innerHTML = '0';
+            selectedCell.style.backgroundColor = '#ffff00';
+        }
+    }
+
+    // third screen
+    function bindGameControlsEvents() {
+        let turn = 1;
+        let ties = 10;
+
+        function handleTurns() {
+            turn++;
+            ties--;
+
+            if (ties == 0) {
+                alert('последний ход');
+            }
+        }
+
+        function handleKeyDown(event) {
+            var keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+            console.log(event);
+
+            if (event.key === 'Escape') {
+                window.location.reload();
+            }
+
+            if (keys.includes(event.key)) {
+                const selectedCell = document.getElementById(`cell${event.key}`);
+
+                if (selectedCell.innerText === '') {
+                    renderPlayerTurn(selectedCell, turn);
+                    handleTurns();
+                    calc();
+                } else {
+                    alert('Поле занято');
+                }
+            }
+
+
+        }
+
+        function handleMouseClick(event) {
+            if (event.target.className == 'cell') {
+                const selectedCell = event.target;
+                console.log(selectedCell.innerText === '');
+                console.log(selectedCell.innerText);
+                if (selectedCell.innerText === '') {
+                    renderPlayerTurn(selectedCell, turn);
+                    handleTurns();
+                    calc();
+                }
+                else {
+                    alert('Поле занято');
+                }
+            }
+        }
+
+        document.addEventListener('keydown', handleKeyDown, true);
+        document.getElementById('game').addEventListener('click', handleMouseClick, true);
+
+        function resetHandlers() {
+            document.removeEventListener('keydown', handleKeyDown, true);
+            document.getElementById('game').removeEventListener('click', handleMouseClick, true);
+        }
+
+        return resetHandlers;
+    }
+
+    function playVideo() {
+        videoContent = document.getElementById('contentElement');
+        playButton = document.getElementById('playButton');
+        playButton.addEventListener('click', onPlayButtonClick);
+
+        function onPlayButtonClick() {
+            videoContent.play();
+        }
+
+        startContainer.style.display = "none";
+        onPlayButtonClick();
+        video.style.left = "0";
+        setTimeout(() => {
+            videoContent.pause();
+            backStyleStart();
+        }, 11000);
+    }
+
+    // INIT GAME
+    initEntryScreen();
+
+
     document.getElementById("playerOneScore").innerHTML =
-    "<br>Score " + playerS.playerOne.score;
-    
+        "<br>Score " + '<span>' + playerS.playerOne.score + '</span>';
+
     document.getElementById("playerTwoScore").innerHTML =
-    "<br>Score " + playerS.playerTwo.score;   
-   
-    videoContent2 = document.getElementById('contentElement2');    
-    playButton2 = document.getElementById('playButton2');    
-    playButton2.addEventListener('click', onPlayButtonClick2);
+    "<br>Score " + '<span>' + playerS.playerTwo.score + '</span>';
+
+    videoContent2 = document.getElementById('contentElement2');
+    restartGame = document.getElementById('restartGame');
+    restartGame.addEventListener('click', onPlayButtonClick2);
 
     function onPlayButtonClick2() {
-        video.style.left="0";
+        video.style.left = "0";
+        videoContent.style.display = 'none';
+        videoContent2.style.display = 'block';
         videoContent2.play();
         console.log('PLAY second');
-    } 
+    }
 
-    rolik = document.getElementById('playButton2');
+    rolik = document.getElementById('restartGame');
     video = document.getElementById('mainContainer');
-    
-    rolik.onclick = function(event) {
-        onPlayButtonClick2();        
-        setInterval(() => {
+
+    rolik.onclick = function (event) {
+        onPlayButtonClick2();
+        setTimeout(() => {
+            videoContent2.pause();
             backStyle();
-        }, 21000);      
-     
+        }, 21000);
+
     }
 
     /// must be delete this style
 
     function backStyle() {
-        video.style.left="-500%";
-        setInterval(() => {
-            restart();
-        }, 1000);        
+        video.style.left = "-500%";
+        resetCellsToDefaultValues();
     }
 
     function backStyleStart() {
-        video.style.left="-500%";
+        video.style.left = "-500%";
         gameIndex = document.getElementById('gameIndex');
-        gameIndex.style.display = "block";              
+        gameIndex.style.display = "block";
     }
 
-    function restart() {
-        window.location.reload();                
-    }   
-    
-    //////////////////////////////////////////////////
-    //////////////////////////////////////////////////
-                  //START OF GAME//
-    //////////////////////////////////////////////////
-    //////////////////////////////////////////////////
+    function resetCellsToDefaultValues() {
+        // document.getElementsByClassName('cell').forEach;
+        var game = document.getElementById('game');
 
-
-    j = 1;
-    nuchya = 10;
-
-    // loopipng cell 
-    for (var cell=1; cell<10; cell++) {        
-        document.getElementById('game').innerHTML+=`<div id="cell${cell}" class="cell" tabindex="1" ></div>`
-    }      
-
-    document.getElementById('game').onclick = function(event) {  
-              
-        if ( event.target.className == 'cell')     {
-            //this.innerHTML = '0';            
-            if (event.target.innerHTML == '') {
-                if (j%2==0) {
-                    event.target.innerHTML = '0';
-                    event.target.style.backgroundColor = '#ffff00';    
-                }    else {
-                    event.target.innerHTML = 'x';           
-                    event.target.style.backgroundColor = '#0099ff';    
-                }     
-              
-                j++;
-                nuchya--;
-
-                if ( nuchya == 0) {
-                    alert ('последний ход');
-                }
-                
-                calc();
-            }
-            else {
-                alert ('Поле занято');
-            }               
+        while (game.firstChild) {
+            game.removeChild(game.firstChild);
         }
-
-        ////////////////////////////////
-            // Calculation of click
-        ////////////////////////////////
-
-        function calc() {
-            let myarray = document.getElementsByClassName('cell') ;    
-
-            // line horizint x
-
-            if ( 
-                (myarray[0].innerHTML== 'x' && myarray[1].innerHTML=='x' && myarray[2].innerHTML == 'x') 
-                ||
-                (myarray[3].innerHTML== 'x' && myarray[4].innerHTML=='x' && myarray[5].innerHTML == 'x') 
-                ||  
-                (myarray[6].innerHTML== 'x' && myarray[7].innerHTML=='x' && myarray[8].innerHTML == 'x')
-                ||
-                (myarray[0].innerHTML== 'x' && myarray[3].innerHTML=='x' && myarray[6].innerHTML == 'x')
-                ||   
-                (myarray[1].innerHTML== 'x' && myarray[4].innerHTML=='x' && myarray[7].innerHTML == 'x')
-                ||
-                (myarray[2].innerHTML== 'x' && myarray[5].innerHTML=='x' && myarray[8].innerHTML == 'x')
-                ||
-                (myarray[0].innerHTML== 'x' && myarray[4].innerHTML=='x' && myarray[8].innerHTML == 'x')
-                ||
-                (myarray[2].innerHTML== 'x' && myarray[4].innerHTML=='x' && myarray[6].innerHTML == 'x')
-            ) 
-            {   
-                document.getElementById("playerOneScore").innerHTML = 1;
-                
-                function func() {
-                    alert( 'Krestiki Победители' );
-                } 
-
-                setTimeout(func, 500);
-
-                // timer not see delay, why
-                //http://plnkr.co/edit/Sh2ovcaQkcNb44TnYZyX?p=preview
-           
-                function myFunction1() {
-                    let element = document.getElementById("myDIV1");
-                    element.classList.add("mystyle");
-                }
-                myFunction1();                
-            }
-            
-            else if (                    
-            //line jorizont 0
-            (myarray[0].innerHTML== '0' && myarray[1].innerHTML=='0' && myarray[2].innerHTML == '0')
-            ||
-            (myarray[3].innerHTML== '0' && myarray[4].innerHTML=='0' && myarray[5].innerHTML == '0')
-            ||
-            (myarray[6].innerHTML== '0' && myarray[7].innerHTML=='0' && myarray[8].innerHTML == '0')
-            ||
-            //line vertical 0
-            (myarray[0].innerHTML== '0' && myarray[3].innerHTML=='0' && myarray[6].innerHTML == '0') 
-            ||
-            (myarray[1].innerHTML== '0' && myarray[4].innerHTML=='0' && myarray[7].innerHTML == '0') 
-            ||
-            (myarray[2].innerHTML== '0' && myarray[5].innerHTML=='0' && myarray[8].innerHTML == '0')
-            ||
-            //diag 0
-            (myarray[0].innerHTML== '0' && myarray[4].innerHTML=='0' && myarray[8].innerHTML == '0')
-            ||
-            (myarray[2].innerHTML== '0' && myarray[4].innerHTML=='0' && myarray[6].innerHTML == '0')
-            )
-            {
-                document.getElementById("playerTwoScore").innerHTML = 1;
-
-                function func() {
-                    alert( 'Noliki Победители' );
-                }
-
-                setTimeout(func, 500);
-
-                function myFunction2() {
-                    let element = document.getElementById("myDIV2");
-                    element.classList.add("mystyle");
-                }                
-                myFunction2();                
-            }                
-           
-            function restart() {
-                window.location.reload();                
-            }
-        }       
+        createCells();
+        resetHandlers();
+        resetHandlers = bindGameControlsEvents();
     }
-
-    
 }
+
 
 
 
